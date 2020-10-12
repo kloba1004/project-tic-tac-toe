270d762577525fb111c24e4deaaa348e15a898c5
kloba1004 <ante.bebic@gmail.com>
2020-10-12 14:44:47 +0200

    bug fixed, now implementing a single player vs AI

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
         form.classList.remove('form-active');
         overlay.classList.remove('overlay-active');
     }
-    
+
     cancelButton.addEventListener('click', () => removeClass());
     overlay.addEventListener('click', () => removeClass());
 
@@ -33,6 +33,54 @@
         form.reset();
         e.preventDefault();
     });
+    let i,
+        player1,
+        player;
+    //pull out squares from the display
+    const squares = Array.from(document.querySelectorAll('.square'));
+
+    const winningCombos = (() => {
+        //list all the possible winning combinations of rows/columns/diagonals into two objects, one for rows and second for rest
+        const rowCombos = {},
+            remainingCombos = {},
+            //number of rows/columns of tic tac toe, thus n equals 3
+            n = 3;
+    
+        //fill first object with rows
+        for (let i = 0; i < n; i++) {
+            rowCombos[`row${i+1}`] = squares.filter(element => 
+                (squares.indexOf(element) < squares.length/n + i*n) && (squares.indexOf(element) >= n*i));
+        }
+    
+        //fill second object with columns
+        for (let i = 0; i < n; i++) {
+            const column = [];
+    
+            for (row in rowCombos) {
+                column.push(rowCombos[row][i]); 
+            }
+            remainingCombos[`column${i+1}`] = column;
+        }
+    
+        //fill second object with diagonals
+        const diagonal1 = [],
+            diagonal2 = [];
+    
+        let counter1 = 0,
+            counter2 = n - 1;
+    
+        for (row in rowCombos) {
+            diagonal1.push(rowCombos[row][counter1]);
+            counter1++;
+    
+            diagonal2.push(rowCombos[row][counter2]);
+            counter2--;
+        }
+        remainingCombos.diagonal1 = diagonal1;
+        remainingCombos.diagonal2 = diagonal2;
+
+        return [rowCombos, remainingCombos];
+    })();
 
     function play(firstPlayer, secondPlayer){
 
@@ -42,64 +90,63 @@
         startButton.textContent = 'restart';
         //create factory function to assign each player object his name, sign and checking of game state to 
         //announce his/her win in case of the  win after every move
+        const announceWinner = (playerName) => {
+            displayResult[0].textContent = playerName;
+            displayResult[1].textContent = 'has';
+            displayResult[2].textContent = 'won!';
+            displayResult.forEach(headline => headline.classList.add('headline-active'));
+            squares.forEach(square => square.removeEventListener('click', displayMove));
+        };
+
+        const checkWin = (name, sign) => {
+            for (row in winningCombos[0]) {
+                const counter = winningCombos[0][row].reduce((counter, square) =>
+                    square.textContent === sign ? counter + 1 : counter, 0);
+                if (counter === 3) {
+                    winningCombos[0][row].forEach(square => square.classList.add('color-change'));
+                    announceWinner(name);
+                } else if ( counter !== 3 && i === 8) announceTie();
+            }
+
+            for (columnOrDiagonal in winningCombos[1]) {
+                const counter = winningCombos[1][columnOrDiagonal].reduce((counter, square) => 
+                    square.textContent === sign ? counter + 1 : counter, 0);
+                if (counter === 3) {
+                    winningCombos[1][columnOrDiagonal].forEach(square => square.classList.add('color-change'));
+                    announceWinner(name);
+                } else if ( counter !== 3 && i === 8) announceTie();
+            }
+        };
+
         function player(name, sign) {
-            
-            const announceWinner = () => {
-                displayResult[0].textContent = name;
-                displayResult[1].textContent = 'has';
-                displayResult[2].textContent = 'won!';
-                displayResult.forEach(headline => headline.classList.add('headline-active'));
-                squares.forEach(square => square.removeEventListener('click', displayMove));
-            };
-
-            const checkWin = () => {
-                for (row in winningCombos[0]) {
-                    const counter = winningCombos[0][row].reduce((counter, square) =>
-                        square.textContent === sign ? counter + 1 : counter, 0);
-                    if (counter === 3) {
-                        winningCombos[0][row].forEach(square => square.classList.add('color-change'));
-                        announceWinner();
-                    } else if ( counter !== 3 && i === 8) announceTie();
-                }
-
-                for (columnOrDiagonal in winningCombos[1]) {
-                    const counter = winningCombos[1][columnOrDiagonal].reduce((counter, square) => 
-                        square.textContent === sign ? counter + 1 : counter, 0);
-                    if (counter === 3) {
-                        winningCombos[1][columnOrDiagonal].forEach(square => square.classList.add('color-change'));
-                        announceWinner();
-                    } else if ( counter !== 3 && i === 8) announceTie();
-                }
-            };
-            return {name, sign, checkWin};
+            return {name, sign};
         }
-        const player1 = player(firstPlayer, 'X');
-        const player2 = player(secondPlayer, 'O');
+        player1 = player(firstPlayer, 'X');
+        player2 = player(secondPlayer, 'O');
+        i = 0;
 
-        //pull out squares from the display
-        const squares = Array.from(document.querySelectorAll('.square'));
         //reset the squares
         squares.map(square => square.textContent = "");
         squares.forEach(square => square.classList.remove('color-change'));
 
         //logic for displaying each players move on screen and swapping between players as well as checking for the winner after each move
-        let i = 0;
         displayTurns[0].textContent = `${player1.name}'s turn`;
         displayTurns[1].textContent = `${player2.name}'s turn`;
         displayTurns[0].classList.add('turn-active');
+        displayTurns[1].classList.remove('turn-active');
 
         function displayMove(e) {
-            if (e.target.textContent === "") {
+            if (e.target.textContent === "" && displayResult[1].textContent === 'vs.') {
                 if (i % 2 === 0) {
                     e.target.textContent = "X";
-                    player1.checkWin();
-                    if (displayResult[1].textContent === 'vs.') displayTurns[1].classList.add('turn-active');
+                    checkWin(player1.name, player1.sign);
+                     displayTurns[1].classList.add('turn-active');
                     displayTurns[0].classList.remove('turn-active');
                 } else { 
                     e.target.textContent = "O";
-                    player2.checkWin();
+                    checkWin(player2.name, player2.sign);
                     displayTurns[1].classList.remove('turn-active');
-                    if (displayResult[1].textContent === 'vs.') displayTurns[0].classList.add('turn-active');
+                    displayTurns[0].classList.add('turn-active');
                 }
                 i++;
             }
@@ -114,48 +161,5 @@
             displayResult.forEach(headline => headline.classList.add('headline-active'));
             squares.forEach(square => square.removeEventListener('click', displayMove));
         }
-
-        const winningCombos = (() => {
-            //list all the possible winning combinations of rows/columns/diagonals into two objects, one for rows and second for rest
-            const rowCombos = {},
-                remainingCombos = {},
-                //number of rows/columns of tic tac toe, thus n equals 3
-                n = 3;
-        
-            //fill first object with rows
-            for (let i = 0; i < n; i++) {
-                rowCombos[`row${i+1}`] = squares.filter(element => 
-                    (squares.indexOf(element) < squares.length/n + i*n) && (squares.indexOf(element) >= n*i));
-            }
-        
-            //fill second object with columns
-            for (let i = 0; i < n; i++) {
-                const column = [];
-        
-                for (row in rowCombos) {
-                    column.push(rowCombos[row][i]); 
-                }
-                remainingCombos[`column${i+1}`] = column;
-            }
-        
-            //fill second object with diagonals
-            const diagonal1 = [],
-                diagonal2 = [];
-        
-            let counter1 = 0,
-                counter2 = n - 1;
-        
-            for (row in rowCombos) {
-                diagonal1.push(rowCombos[row][counter1]);
-                counter1++;
-        
-                diagonal2.push(rowCombos[row][counter2]);
-                counter2--;
-            }
-            remainingCombos.diagonal1 = diagonal1;
-            remainingCombos.diagonal2 = diagonal2;
-
-            return [rowCombos, remainingCombos];
-        })();
     }
 })();
